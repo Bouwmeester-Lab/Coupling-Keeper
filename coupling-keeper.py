8e028a901c33a59f83d2719ea2b9cae68b2c2400
LeendertT420 <geert.timmerman@hotmail.nl>
2024-06-07 15:01:11 +0200

    clean up

---
+++ coupling-keeper.py
@@ -0,0 +1,12 @@
+from pylablib.devices import Attocube
+import numpy as np
+import time
+from pyvisa import ResourceManager, constants
+import pandas as pd
+
+
+class AgilentScope
+
+
+class CouplingKeeper():
+    def __init__(self, )
